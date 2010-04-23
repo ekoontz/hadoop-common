4bfe4c658c8b59981972db5a1be0ef70dc9d7a15
Vinod Kumar <vinodkv@spacestar-lr.eglbp.corp.yahoo.com>
2010-04-23 15:46:55 +0530

    MAPREDUCE:1397. From https://issues.apache.org/jira/secure/attachment/12442657/patch-1397-ydist.txt

--- src/mapred/org/apache/hadoop/mapred/TaskTracker.java
+++ src/mapred/org/apache/hadoop/mapred/TaskTracker.java
@@ -264,7 +264,7 @@ public class TaskTracker
   private MapEventsFetcherThread mapEventsFetcher;
   int workerThreads;
   CleanupQueue directoryCleanupThread;
-  volatile JvmManager jvmManager;
+  private volatile JvmManager jvmManager;
   
   private TaskMemoryManagerThread taskMemoryManager;
   private boolean taskMemoryManagerEnabled = true;
@@ -1954,7 +1954,12 @@ public class TaskTracker
   public JvmManager getJvmManagerInstance() {
     return jvmManager;
   }
-  
+
+  // called from unit test  
+  void setJvmManagerInstance(JvmManager jvmManager) {
+    this.jvmManager = jvmManager;
+  }
+
   private void addToTaskQueue(LaunchTaskAction action) {
     if (action.getTask().isMapTask()) {
       mapLauncher.addToTaskQueue(action);
@@ -3582,6 +3587,16 @@ public class TaskTracker
   
   int getMaxCurrentReduceTasks() {
     return maxReduceSlots;
+  }
+
+  //called from unit test
+  synchronized void setMaxMapSlots(int mapSlots) {
+    maxMapSlots = mapSlots;
+  }
+
+  //called from unit test
+  synchronized void setMaxReduceSlots(int reduceSlots) {
+    maxReduceSlots = reduceSlots;
   }
 
   /**
