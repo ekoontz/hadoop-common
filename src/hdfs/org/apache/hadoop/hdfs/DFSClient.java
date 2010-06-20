50540fbca7c00479f54459fd194a0c1dbf8d41c9
Dhruba Borthakur <dhruba@apache.org>
2010-06-20 08:31:54 +0000

    HDFS-1118. Fix socketleak on DFSClient. (Zheng Shao via dhruba)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-0.20-append@956329 13f79535-47bb-0310-9956-ffa450edef68

--- src/hdfs/org/apache/hadoop/hdfs/DFSClient.java
+++ src/hdfs/org/apache/hadoop/hdfs/DFSClient.java
@@ -3070,6 +3070,7 @@ public class DFSClient implements FSConstants, java.io.Closeable {
       // persist blocks on namenode on next flush
       persistBlocks = true;
 
+      boolean result = false;
       try {
         LOG.debug("Connecting to " + nodes[0].getName());
         InetSocketAddress target = NetUtils.createSocketAddr(nodes[0].getName());
@@ -3121,7 +3122,7 @@ public class DFSClient implements FSConstants, java.io.Closeable {
         }
 
         blockStream = out;
-        return true;     // success
+        result = true;     // success
 
       } catch (IOException ie) {
 
@@ -3139,8 +3140,14 @@ public class DFSClient implements FSConstants, java.io.Closeable {
         hasError = true;
         setLastException(ie);
         blockReplyStream = null;
-        return false;  // error
+        result = false;
+      } finally {
+        if (!result) {
+          IOUtils.closeSocket(s);
+          s = null;
+        }
       }
+      return result;
     }
   
     private LocatedBlock locateFollowingBlock(long start,
