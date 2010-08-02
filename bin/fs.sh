0fb118e4a409cc7d84ac2fedce502ea95f953091
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-08-02 09:51:42 -0700

    remove unneeded $FLAGS.

--- bin/fs.sh
+++ bin/fs.sh
@@ -6,5 +6,5 @@ export CLASSPATH="/Users/ekoontz/hadoop-hdfs/build/classes:\
 /Users/ekoontz/hadoop-common/build/ivy/lib/Hadoop-Common/common/commons-lang-2.5.jar:\
 /Users/ekoontz/hadoop-common/build/ivy/lib/Hadoop-Common/common/commons-logging-1.1.1.jar"
 
-java $FLAGS org.apache.hadoop.fs.FsShell $*
+java org.apache.hadoop.fs.FsShell $*
 
