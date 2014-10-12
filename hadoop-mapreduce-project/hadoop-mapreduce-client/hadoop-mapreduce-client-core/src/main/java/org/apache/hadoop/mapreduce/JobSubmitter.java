2b2b6c14c0dce31ed8b21e2720b58e0a6dd7102b
Karthik Kambatla <kasha@apache.org>
2014-10-11 22:48:47 -0700

    MAPREDUCE-5875. Make Counter limits consistent across JobClient, MRAppMaster, and YarnChild. (Gera Shegalov via kasha)
    (cherry picked from commit e8a31f2e1c34514fba2f480e8db652f6e2ed65d8)
    
    Conflicts:
    	hadoop-mapreduce-project/CHANGES.txt

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/JobSubmitter.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/JobSubmitter.java
@@ -51,6 +51,7 @@ import org.apache.hadoop.mapred.QueueACL;
 
 import static org.apache.hadoop.mapred.QueueManager.toFullPropertyName;
 
+import org.apache.hadoop.mapreduce.counters.Limits;
 import org.apache.hadoop.mapreduce.filecache.ClientDistributedCacheManager;
 import org.apache.hadoop.mapreduce.filecache.DistributedCache;
 import org.apache.hadoop.mapreduce.protocol.ClientProtocol;
@@ -528,6 +529,7 @@ class JobSubmitter {
 
       // Write job file to submit dir
       writeConf(conf, submitJobFile);
+      Limits.reset(conf);
       
       //
       // Now, actually submit the job (using the submit name)
