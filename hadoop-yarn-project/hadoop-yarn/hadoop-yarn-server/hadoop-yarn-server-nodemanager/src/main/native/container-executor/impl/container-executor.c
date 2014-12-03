52acfecc48ad16ba1b7529d4f950a64ea596e8d9
Harsh J <harsh@cloudera.com>
2014-12-04 03:16:08 +0530

    YARN-2891. Failed Container Executor does not provide a clear error message. Contributed by Dustin Cote. (harsh)
    (cherry picked from commit a31e0164912236630c485e5aeb908b43e3a67c61)
    
    Change-Id: I80aa674bf7551f974875f26801c7f60a0dc19657

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/native/container-executor/impl/container-executor.c
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/native/container-executor/impl/container-executor.c
@@ -550,7 +550,7 @@ int check_dir(const char* npath, mode_t st_mode, mode_t desired, int finalCompon
     int filePermInt = st_mode & (S_IRWXU | S_IRWXG | S_IRWXO);
     int desiredInt = desired & (S_IRWXU | S_IRWXG | S_IRWXO);
     if (filePermInt != desiredInt) {
-      fprintf(LOGFILE, "Path %s does not have desired permission.\n", npath);
+      fprintf(LOGFILE, "Path %s has permission %o but needs permission %o.\n", npath, filePermInt, desiredInt);
       return -1;
     }
   }
