f77fdbbbec3387886c37df6bf86cac64d20a1c5b
Vinayakumar B <vinayakumarb@apache.org>
2015-03-04 14:38:38 +0530

    HDFS-7869. Inconsistency in the return information while performing rolling upgrade ( Contributed by J.Andreina )
    (cherry picked from commit 3560180b6e9926aa3ee1357da59b28a4b4689a0d)
    (cherry picked from commit c24448c27b91d0398fb42a0a9ab8b8cdd4887647)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/CHANGES.txt

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -8512,7 +8512,7 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
     }
   }
 
-  void finalizeRollingUpgrade() throws IOException {
+  RollingUpgradeInfo finalizeRollingUpgrade() throws IOException {
     checkSuperuserPrivilege();
     checkOperation(OperationCategory.WRITE);
     writeLock();
@@ -8520,7 +8520,7 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
     try {
       checkOperation(OperationCategory.WRITE);
       if (!isRollingUpgrade()) {
-        return;
+        return null;
       }
       checkNameNodeSafeMode("Failed to finalize rolling upgrade");
 
@@ -8545,7 +8545,7 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
     if (auditLog.isInfoEnabled() && isExternalInvocation()) {
       logAuditEvent(true, "finalizeRollingUpgrade", null, null, null);
     }
-    return;
+    return returnInfo;
   }
 
   RollingUpgradeInfo finalizeRollingUpgradeInternal(long finalizeTime)
