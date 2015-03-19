1e946372cf81d124a0c3630b1f7cc6f30dd4e7a7
Vinayakumar B <vinayakumarb@apache.org>
2015-03-19 11:59:31 +0530

    HDFS-7869. Update action param from 'start' to 'prepare' in rolling upgrade ( Contributed by J.Andreina)
    (cherry picked from commit 338b477ff736cb6c8fb8204ab83fb038594dadf0)
    (cherry picked from commit 4527aa6f77ec817219951ddb6992d66fdf64fe91)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/CHANGES.txt

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DistributedFileSystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DistributedFileSystem.java
@@ -1093,7 +1093,7 @@ public class DistributedFileSystem extends FileSystem {
   }
 
   /**
-   * Rolling upgrade: start/finalize/query.
+   * Rolling upgrade: prepare/finalize/query.
    */
   public RollingUpgradeInfo rollingUpgrade(RollingUpgradeAction action)
       throws IOException {
