c78f386ec3f565ce98791e7a1bc2cd7768cb4317
Wangda Tan <wangda@apache.org>
2015-04-17 17:11:22 -0700

    YARN-3493. RM fails to come up with error "Failed to load/recover state" when mem settings are changed. (Jian He via wangda)
    (cherry picked from commit f65eeb412d140a3808bcf99344a9f3a965918f70)
    (cherry picked from commit e7cbecddc3e7ca5386c71aa4deb67f133611415c)
    (cherry picked from commit 9d47d5aa5bffe427c4a77260f7ccc039d446e1fd)
    (cherry picked from commit 61f2ddb125f6af1775ce5d219f22eb079071869c)
    
    Conflicts:
    	hadoop-yarn-project/CHANGES.txt
    	hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/rmapp/attempt/RMAppAttemptImpl.java
    	hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/TestWorkPreservingRMRestart.java
    
    Change-Id: I40a82675202ff01a05aead7846ae44d2bb7e2260
    (cherry picked from commit b3c79180ff4cb2fb84125c9ea0101ce17c2d8eaf)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/ApplicationMasterService.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/ApplicationMasterService.java
@@ -501,7 +501,7 @@ public class ApplicationMasterService extends AbstractService implements
               
       // sanity check
       try {
-        RMServerUtils.validateResourceRequests(ask,
+        RMServerUtils.normalizeAndValidateRequests(ask,
             rScheduler.getMaximumResourceCapability(), app.getQueue(),
             rScheduler);
       } catch (InvalidResourceRequestException e) {
