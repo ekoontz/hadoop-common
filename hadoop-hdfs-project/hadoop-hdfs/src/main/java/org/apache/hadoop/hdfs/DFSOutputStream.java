f132afedb99a2fa691063e151df6b95f2675f645
Yongjun Zhang <yzhang@cloudera.com>
2015-05-08 00:11:46 -0700

    HDFS-8311. DataStreamer.transfer() should timeout the socket InputStream. (Esteban Gutierrez via Yongjun Zhang)
    (cherry picked from commit 730f9930a48259f34e48404aee51e8d641cc3d36)
    (cherry picked from commit fbb2cf54bcaa4ce739747e04fce4058b2639a619)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DataStreamer.java
    
    Change-Id: I0d8e96aa5655b9a504a08e92fc33a5b9aff27e22

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSOutputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSOutputStream.java
@@ -1244,9 +1244,10 @@ public class DFSOutputStream extends FSOutputSummer
       try {
         sock = createSocketForPipeline(src, 2, dfsClient);
         final long writeTimeout = dfsClient.getDatanodeWriteTimeout(2);
+        final long readTimeout = dfsClient.getDatanodeReadTimeout(2);
         
         OutputStream unbufOut = NetUtils.getOutputStream(sock, writeTimeout);
-        InputStream unbufIn = NetUtils.getInputStream(sock);
+        InputStream unbufIn = NetUtils.getInputStream(sock, readTimeout);
         IOStreamPair saslStreams = dfsClient.saslClient.socketSend(sock,
           unbufOut, unbufIn, dfsClient, blockToken, src);
         unbufOut = saslStreams.out;
@@ -1529,9 +1530,10 @@ public class DFSOutputStream extends FSOutputSummer
           assert null == blockReplyStream : "Previous blockReplyStream unclosed";
           s = createSocketForPipeline(nodes[0], nodes.length, dfsClient);
           long writeTimeout = dfsClient.getDatanodeWriteTimeout(nodes.length);
+          long readTimeout = dfsClient.getDatanodeReadTimeout(nodes.length);
           
           OutputStream unbufOut = NetUtils.getOutputStream(s, writeTimeout);
-          InputStream unbufIn = NetUtils.getInputStream(s);
+          InputStream unbufIn = NetUtils.getInputStream(s, readTimeout);
           IOStreamPair saslStreams = dfsClient.saslClient.socketSend(s,
             unbufOut, unbufIn, dfsClient, accessToken, nodes[0]);
           unbufOut = saslStreams.out;
