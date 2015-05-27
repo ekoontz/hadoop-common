7c8ed8723ba46131b35c09a9157a25cce7f72f19
Akira Ajisaka <aajisaka@apache.org>
2015-05-27 18:04:40 +0900

    HADOOP-11242. Record the time of calling in tracing span of IPC server. Contributed by Mastake Iwasaki.
    (cherry picked from commit bb1816328a36ec3f8c6bd9fdb950d9a4ec8388c8)
    (cherry picked from commit 9670762a82a3c63bdfe6cd4feb86025ad640a56e)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
@@ -2026,6 +2026,7 @@ public abstract class Server {
           CurCall.set(call);
           if (call.traceSpan != null) {
             traceScope = Trace.continueSpan(call.traceSpan);
+            traceScope.getSpan().addTimelineAnnotation("called");
           }
 
           try {
