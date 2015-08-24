446c6d83c8be091a4753eb91e4c5ee9378bbc1e4
Karthik Kambatla <kasha@cloudera.com>
2015-08-24 14:55:10 -0700

    Add post-commit-hook
    Change-Id: Ieae1d02223a708142feba939da70b0b355669e80

---
+++ cloudera/post_commit_hook.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -xe
+
+SCRIPTS="jenkins-job-scripts"
+DIR="$( cd $( dirname $( dirname ${BASH_SOURCE[0]} ) ) && pwd )"
+cd $DIR
+
+if [[ -d $SCRIPTS ]]; then
+    echo "Cleaning up remnants from a previous run"
+    rm -rf $SCRIPTS
+fi
+
+# Clone the jenkins script repo
+git clone --depth 1 git://github.mtv.cloudera.com/CDH/$SCRIPTS.git $SCRIPTS || true
+
+# Fetch the right branch
+cd $SCRIPTS
+git fetch --depth 1 origin
+git checkout -f origin/master
+git ls-tree -r HEAD
+cd ..
+
+# Run the build and tests
+export CONF_NAME=CDH5-Hadoop-2.6.0
+./jenkins-job-scripts/run.sh
+
+if [[ -d $SCRIPTS ]]; then
+    rm -rf $SCRIPTS
+fi
