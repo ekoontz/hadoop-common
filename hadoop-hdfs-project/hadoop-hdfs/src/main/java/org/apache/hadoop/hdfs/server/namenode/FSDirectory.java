7ef664f614d02a27720edfc851bffa3d14386c10
Yongjun Zhang <yzhang@cloudera.com>
2015-11-05 14:36:12 -0800

    HDFS-9377. Fix findbugs warnings in FSDirSnapshotOp. (Mingliang Liu via Yongjun Zhang)
    (cherry picked from commit 286cc6483fe458894cdd22ff1d8822c304e0d94f)
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSDirSnapshotOp.java
    
    Change-Id: I2efb510325fe30581bcabe6dfcd8c086a3ae6ee5

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSDirectory.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSDirectory.java
@@ -1551,8 +1551,7 @@ public class FSDirectory implements Closeable {
   Collection<String> getSnapshotFiles(
       List<DirectorySnapshottableFeature> lsf,
       String file) throws IOException {
-    ArrayList<String> snaps = new ArrayList<String>();
-    ListIterator<DirectorySnapshottableFeature> sfi = lsf.listIterator();
+    ArrayList<String> snaps = new ArrayList<>();
     for (DirectorySnapshottableFeature sf : lsf) {
       // for each snapshottable dir e.g. /dir1, /dir2
       final ReadOnlyList<Snapshot> lsnap = sf.getSnapshotList();
